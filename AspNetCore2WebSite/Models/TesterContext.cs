a11a2a567d97b9f77ad8a10e4849e3c57c0d8150
Brett Morin <brettmorin2@gmail.com>
2017-11-13 09:24:26 -0800

    Added code for WebSite and got controller working

---
+++ AspNetCore2WebSite/Models/TesterContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AspNetCore2WebSite.Models
+{
+    public partial class TesterContext : DbContext
+    {
+        public virtual DbSet<Person> Person { get; set; }
+
+        public TesterContext(DbContextOptions<TesterContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(64)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(64)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
